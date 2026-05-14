7d8334e0049b0d24e54080c6a31f39a3df5899bd
wildfont <jordifont4@gmail.com>
2026-05-14 17:10:46 +0200

    done day 4

--- server/app.js
+++ server/app.js
@@ -22,7 +22,11 @@ const app = express();
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
 // ...
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+  }),
+);
 
 app.use(express.json());
 app.use(morgan("dev"));
@@ -39,194 +43,194 @@ app.get("/docs", (req, res) => {
 
 //const cohorts = require("./cohorts.json")
 app.get("/api/cohorts", async (req, res) => {
-   try{
-  const response = await Cohort.find()
-  console.log(response)
- res.json(response)
- } catch (error) {
-  console.log(error)
- }
+  try {
+    const response = await Cohort.find();
+    res.status(200).json(response);
+    res.json(response);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-
 app.post("/api/cohorts", (req, res) => {
-  
+  console.log(req.body);
 
-
-console.log(req.body)
-
-
-const newCohort = {
-  inProgress: req.body.inProgress,
-cohortSlug: req.body.cohortSlug,
-cohortName: req.body.cohortName,
-program: req.body.program,
-campus: req.body.campus,
-startDate: req.body.startDate,
-endDate: req.body.endDate,
-programManager: req.body.programManager,
-leadTeacher: req.body.leadTeacher,
-totalHours: req.body.totalHours 
-}
-Cohort.create(newCohort)
-.then(() => {
-  res.send("Cohort Created")
-})
-.catch((error) => {
-  console.log(error)
-})
-
-})
+  const newCohort = {
+    inProgress: req.body.inProgress,
+    cohortSlug: req.body.cohortSlug,
+    cohortName: req.body.cohortName,
+    program: req.body.program,
+    campus: req.body.campus,
+    startDate: req.body.startDate,
+    endDate: req.body.endDate,
+    programManager: req.body.programManager,
+    leadTeacher: req.body.leadTeacher,
+    totalHours: req.body.totalHours,
+  };
+  Cohort.create(newCohort)
+    .then(() => {
+      res.send("Cohort Created");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
 
 app.get("/api/cohorts/:cohortId", async (req, res) => {
- try{
-  const response = await Cohort.findById(req.params.cohortId)
-  console.log(response)
- res.json(response)
- } catch (error) {
-  console.log(error)
- }
-})
+  try {
+    const response = await Cohort.findById(req.params.cohortId);
+    res.status(200).json(response);
+  } catch (error) {
+    next(error);
+  }
+});
 
 app.put("/api/cohorts/:cohortId", async (req, res, next) => {
+  console.log(req.params);
+  console.log(req.body);
 
-  console.log(req.params)
-  console.log(req.body)
+  try {
+    const updatedCohort = {
+      inProgress: req.body.inProgress,
+      cohortSlug: req.body.cohortSlug,
+      cohortName: req.body.cohortName,
+      program: req.body.program,
+      campus: req.body.campus,
+      startDate: req.body.startDate,
+      endDate: req.body.endDate,
+      programManager: req.body.programManager,
+      leadTeacher: req.body.leadTeacher,
+      totalHours: req.body.totalHours,
+    };
+    const response = await Cohort.findByIdAndUpdate(
+      req.params.cohortId,
+      updatedCohort,
+    );
 
-  try{
-
-const updatedCohort = {
-   inProgress: req.body.inProgress,
-cohortSlug: req.body.cohortSlug,
-cohortName: req.body.cohortName,
-program: req.body.program,
-campus: req.body.campus,
-startDate: req.body.startDate,
-endDate: req.body.endDate,
-programManager: req.body.programManager,
-leadTeacher: req.body.leadTeacher,
-totalHours: req.body.totalHours 
-  
-}
-const response = await Cohort.findByIdAndUpdate(req.params.cohortId, updatedCohort)
-
-  res.send("cohort updated")
+    res.status(202).send("cohort updated");
   } catch (error) {
-    console.log(error)
+    next(error);
   }
-})
+});
 
-
-app.delete("/api/cohorts", async (req, res) => {
- try{
-  const response = await Cohort.findByIdAndDelete(req.params.cohortId)
-  console.log(response)
- res.json(response)
- } catch (error) {
-  console.log(error)
- }
-})
+app.delete("/api/cohorts/:cohortId", async (req, res) => {
+  try {
+    const response = await Cohort.findByIdAndDelete(req.params.cohortId);
+    res.status(202).json(response);
+  } catch (error) {
+    next(error);
+  }
+});
 
 //const students = require("./students.json")
 app.get("/api/students", async (req, res) => {
-   try{
-  const response = await Student.find()
-  console.log(response)
- res.json(response)
- } catch (error) {
-  console.log(error)
- }
+  try {
+    const response = await Student.find().populate("cohort");
+    res.status(200).json(response);
+  } catch (error) {
+    next(error);
+  }
 });
 
 app.post("/api/students", (req, res) => {
-  
+  //1. we need to receive a body
+  console.log(req.body);
+  //2. we need to go into the db to create something
 
-//1. we need to receive a body
-console.log(req.body)
-//2. we need to go into the db to create something
-
-const newStudent = {
-  firstName: req.body.firstName,
-  lastName: req.body.lastName,
-  email: req.body.email,
-  phone: req.body.phone,
-  linkedinUrl: req.body.linkedinUrl,
-  languages: req.body.languages,
-  program: req.body.program,
-  background: req.body.background,
-  image: req.body.image,
-  projects: req.body.projects,
-  cohort: req.body.cohort
-  
-}
-Student.create(newStudent)
-.then(() => {
-  res.send("Student created")
-})
-.catch((error) => {
-  console.log(error)
-})
-
-})
+  const newStudent = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    phone: req.body.phone,
+    linkedinUrl: req.body.linkedinUrl,
+    languages: req.body.languages,
+    program: req.body.program,
+    background: req.body.background,
+    image: req.body.image,
+    projects: req.body.projects,
+    cohort: req.body.cohort,
+  };
+  Student.create(newStudent)
+    .then(() => {
+      res.sendStatus(201);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
 
 app.get("/api/students/:studentId", async (req, res) => {
- try{
-  const response = await Student.findById(req.params.studentId).populate('cohort')
-  console.log(response)
- res.json(response)
- } catch (error) {
-  console.log(error)
- }
-})
+  try {
+    const response = await Student.findById(req.params.studentId).populate(
+      "cohort",
+    );
+    res.status(200).json(response);
+  } catch (error) {
+    next(error);
+  }
+});
 
 app.put("/api/students/:studentId", async (req, res, next) => {
+  console.log(req.params);
+  console.log(req.body);
 
-  console.log(req.params)
-  console.log(req.body)
+  try {
+    const updatedStudent = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      phone: req.body.phone,
+      linkedinUrl: req.body.linkedinUrl,
+      languages: req.body.languages,
+      program: req.body.program,
+      background: req.body.background,
+      image: req.body.image,
+      projects: req.body.projects,
+    };
+    const response = await Student.findByIdAndUpdate(
+      req.params.studentId,
+      updatedStudent,
+    );
 
-  try{
-
-const updatedStudent = {
-    firstName: req.body.firstName,
-  lastName: req.body.lastName,
-  email: req.body.email,
-  phone: req.body.phone,
-  linkedinUrl: req.body.linkedinUrl,
-  languages: req.body.languages,
-  program: req.body.program,
-  background: req.body.background,
-  image: req.body.image,
-  projects: req.body.projects,
- 
-  
-}
-const response = await Student.findByIdAndUpdate(req.params.studentId, updatedStudent)
-
-  res.send("student updated")
+    res.sendStatus(202).send("student updated");
   } catch (error) {
-    console.log(error)
+    next(error);
   }
-})
+});
 
 app.get("/api/students/cohort/:cohortId", async (req, res, next) => {
-  try{
-    const studentsBelongCohort = await Student.find({cohort: req.params.cohortId}).populate('cohort')
-    console.log("students belong to cohort")
-    res.json(studentsBelongCohort)
+  try {
+    const studentsBelongCohort = await Student.find({
+      cohort: req.params.cohortId,
+    }).populate("cohort");
+    res.status(200).json(studentsBelongCohort);
   } catch (error) {
-    console.log(error)
+    next(error);
   }
-})
+});
 
-app.delete("/api/students", async (req, res) => {
- try{
-  const response = await Cohort.findByIdAndDelete(req.params.studentId)
-  console.log(response)
- res.json(response)
- } catch (error) {
-  console.log(error)
- }
-})
+app.delete("/api/students/:studentId", async (req, res) => {
+  try {
+    const response = await Student.findByIdAndDelete(req.params.studentId);
+    res.status(200).json(response);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// error handling for 404 errors
+app.use((req, res) => {
+  res.status(404).json({ errorMessage: "Sorry, route not found" });
+});
+
+// error handling for 500 errors
+app.use((error, req, res, next) => {
+  // express know this is the 500 error handler just because it has 4 parameters.
+  console.log(error);
+  res
+    .status(500)
+    .json({ errorMessage: "something went BOOM, sorry about this" });
+});
 
 // START SERVER
 app.listen(PORT, () => {
